6e1fa42f00b6e3289fe769f806ecf7aadb2aeb48
Revo Arya <44845508+Verestra@users.noreply.github.com>
2021-03-04 09:10:01 +0700

    Create Nomor3.js

---
+++ Nomor3/Nomor3.js
@@ -0,0 +1,28 @@
+function divideAndShort(num){    
+    let tempArray = []; 
+    let numArray = num.toString().split(''); 
+    let resultNum = ''; 
+
+    for(i=0;i<numArray.length;i++){                
+        if(numArray[i] == 0){            
+            tempArray.sort((a,b) => a-b);
+            resultNum += tempArray.join(''); 
+            tempArray = []; 
+            // console.log(resultNum)
+        }
+        else if(i == (numArray.length-1)){
+            if(numArray[i] !== 0){
+                tempArray[i] = numArray[i];
+                // console.log(tempArray)
+            }
+            tempArray.sort((a,b) => a-b);
+            resultNum += tempArray.join(''); 
+            tempArray = [];
+        }else{
+            tempArray[i] = numArray[i]; 
+        }
+    }
+
+    return resultNum
+  }
+  console.log(divideAndShort(5956560159466056))
